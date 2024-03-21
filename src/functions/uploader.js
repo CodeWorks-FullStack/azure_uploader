5e0b86fb76a900c24e8665ee98326c377f6ed9b2
Jake Overall <joverall22@gmail.com>
2024-03-21 10:54:06 -0600

    basic file upload

--- src/functions/uploader.js
+++ src/functions/uploader.js
@@ -1,13 +1,34 @@
 const { app } = require('@azure/functions');
+const multipart = require('parse-multipart')
 
 app.http('uploader', {
-    methods: ['GET', 'POST'],
+    methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+        try {
+            const buffer = await request.arrayBuffer()
+            const binaryFiles = Buffer.from(buffer)
+            const boundry = multipart.getBoundary(request.headers.get('Content-Type'))
 
-        const name = request.query.get('name') || await request.text() || 'world';
+            const files = multipart.Parse(binaryFiles, boundry)
+            context.log('files', files)
 
-        return { body: `Hello, ${name}!` };
+            if (files.length == 0) {
+                return { body: 'Nothing to upload' }
+            }
+
+
+            return { body: 'cool' }
+
+        } catch (error) {
+            return {
+                status: 400,
+                body: error.message
+            }
+        }
+
+
+
+
     }
 });
