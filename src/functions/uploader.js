6f2176b418aef2bc9a73689e28575f29da45ad8e
Jake Overall <joverall22@gmail.com>
2024-03-21 11:26:52 -0600

    testing uploads

--- src/functions/uploader.js
+++ src/functions/uploader.js
@@ -1,5 +1,6 @@
 const { app } = require('@azure/functions');
 const multipart = require('parse-multipart')
+const { BlobServiceClient } = require('@azure/storage-blob')
 
 app.http('uploader', {
     methods: ['POST'],
@@ -17,8 +18,48 @@ app.http('uploader', {
                 return { body: 'Nothing to upload' }
             }
 
+            const uploadables = []
 
-            return { body: 'cool' }
+            files.forEach(f => {
+                // TODO skip if its a bad fileType
+                uploadables.push({
+                    file: f,
+                    fileName: f.filename,
+                    fileType: f.type,
+                    length: f.data.byteLength,
+                    size_kb: f.data.byteLength / 1024,
+                    size_mb: f.data.byteLength / 102400,
+                    url: ''
+                })
+            })
+
+
+            // verify container
+            const blobStorage = BlobServiceClient.fromConnectionString(process.env.AzureWebJobsStorage)
+
+            const container = blobStorage.getContainerClient('public')
+            await container.createIfNotExists({ access: 'blob' })
+
+            // upload blockblobs (aka files)
+            uploadables.forEach(f => {
+                const blockBlob = container.getBlockBlobClient(`/images/${Date.now()}_${f.fileName}`)
+                blockBlob.upload(f.file, f.length, {
+                    blobHTTPHeaders: {
+                        blobContentType: f.type,
+                        blobCacheControl: 'max-age=36000'
+                    }
+                })
+                f.url = blockBlob.url
+            })
+
+
+
+            return {
+                status: 200,
+                body: uploadables.map(f => {
+                    return { url: f.url, size_kb: f.size_kb, size_mb: f.size_mb }
+                })
+            }
 
         } catch (error) {
             return {
